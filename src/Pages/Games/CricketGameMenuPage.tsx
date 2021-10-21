843a538a803893eba2fa438088c671e3231a9781
Jonathon Richardson <jonathon.m.richardson@gmail.com>
2021-10-20 19:46:11 -0500

    WIP
    (cherry picked from commit 46f47fd458df7c09e527c103ec1d496ac5b68261)

--- src/Pages/Games/CricketGameMenuPage.tsx
+++ src/Pages/Games/CricketGameMenuPage.tsx
@@ -1,28 +1,190 @@
 import * as React from "react";
 import "./X01GamesPage.scss";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { getBlankScore, LocalStorageGameDB } from "../../data/GamesDB";
+import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { LocalStoragePlayerDB } from "../../data/PlayerDB";
+import moment from "moment";
 
 interface IProps {}
 
-interface IState {}
+interface IState {
+    playersForGame?: string[];
+}
 
 export class CricketGameMenuPage extends React.Component<IProps, IState> {
-  render() {
-    return (
-      <div>
-        <h2>
-          <Link to="/">&lt; Home</Link>
-        </h2>
-        <ul className="tg-list">
-          <li className="tg-list-item">
-            <h4>Double In</h4>
-            <input id="cb1" className="tgl tgl-light" type="checkbox" />
-            <label className="tgl-btn" htmlFor="cb1" />
-          </li>
-        </ul>
+    state: IState = {};
 
-        <Link to="/games/cricket/1">Play Game</Link>
-      </div>
-    );
-  }
+    render() {
+        let db = new LocalStorageGameDB();
+        let games = db.getGames();
+        let playersDB = new LocalStoragePlayerDB();
+
+        return (
+            <div>
+                <h2>
+                    <Link to="/">&lt; Home</Link>
+                </h2>
+                <ul className="tg-list">
+                    <li className="tg-list-item">
+                        <h4>Double In</h4>
+                        <input
+                            id="cb1"
+                            className="tgl tgl-light"
+                            type="checkbox"
+                        />
+                        <label className="tgl-btn" htmlFor="cb1" />
+                    </li>
+                </ul>
+
+                {games.map((game) => {
+                    return (
+                        <Card style={{ width: "18rem" }}>
+                            <Card.Body>
+                                <Card.Title>{game.id}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">
+                                    {moment(game.startedAt).fromNow()}
+                                </Card.Subtitle>
+                                <Card.Text>
+                                    {game.players
+                                        .map((p) => p.playerInfo.name)
+                                        .join(", ")}
+                                </Card.Text>
+
+                                <Link
+                                    className="btn btn-success"
+                                    to={`/games/cricket/${game.id}`}
+                                >
+                                    Play Game
+                                </Link>
+                                {/* <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link> */}
+                            </Card.Body>
+                        </Card>
+                    );
+                })}
+
+                <Modal show={!!this.state.playersForGame}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>New Game</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <Form>
+                            {playersDB.getPlayers().map((player) => {
+                                return (
+                                    <div key={player.id} className="mb-3">
+                                        <Form.Check
+                                            type="checkbox"
+                                            label={player.name}
+                                            id={player.id}
+                                            checked={
+                                                this.state.playersForGame &&
+                                                !!this.state.playersForGame.find(
+                                                    (x) => x == player.id
+                                                )
+                                            }
+                                            onClick={() => {
+                                                this.setState((curState) => {
+                                                    let currentPlayers =
+                                                        curState.playersForGame
+                                                            ? [
+                                                                  ...curState.playersForGame,
+                                                              ]
+                                                            : [];
+
+                                                    if (
+                                                        currentPlayers.find(
+                                                            (x) =>
+                                                                x == player.id
+                                                        )
+                                                    ) {
+                                                        currentPlayers =
+                                                            currentPlayers.filter(
+                                                                (x) =>
+                                                                    x !==
+                                                                    player.id
+                                                            );
+                                                    } else {
+                                                        currentPlayers.push(
+                                                            player.id
+                                                        );
+                                                    }
+
+                                                    return {
+                                                        playersForGame:
+                                                            currentPlayers,
+                                                    };
+                                                });
+                                            }}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </Form>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button
+                            variant="secondary"
+                            onClick={() => {
+                                this.setState({
+                                    playersForGame: undefined,
+                                });
+                            }}
+                        >
+                            Close
+                        </Button>
+                        <Button
+                            variant="primary"
+                            disabled={
+                                !this.state.playersForGame ||
+                                this.state.playersForGame.length == 0
+                            }
+                            onClick={() => {
+                                if (this.state.playersForGame) {
+                                    db.saveGame({
+                                        id: `${db.getGames().length}`,
+                                        startedAt: new Date(),
+                                        turnNumber: 0,
+                                        turns: [[]],
+                                        players: this.state.playersForGame.map(
+                                            (id) => {
+                                                return {
+                                                    playerInfo: playersDB
+                                                        .getPlayers()
+                                                        .find(
+                                                            (x) => x.id === id
+                                                        ),
+                                                    score: getBlankScore(),
+                                                };
+                                            }
+                                        ),
+                                    });
+                                    this.setState({
+                                        playersForGame: undefined,
+                                    });
+                                }
+                            }}
+                        >
+                            Save changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <Button
+                    onClick={() => {
+                        this.setState({
+                            playersForGame: [],
+                        });
+                    }}
+                >
+                    New Game
+                </Button>
+            </div>
+        );
+    }
 }
--- src/Pages/Games/CricketGamePlayPage.tsx
+++ src/Pages/Games/CricketGamePlayPage.tsx
@@ -3,245 +3,323 @@ import "./X01GamesPage.scss";
 import { Link } from "react-router-dom";
 import { IDartValue, ISegmentType } from "../../Components/Segment";
 import { Dartboard } from "../../Components/Dartboard";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import {
+    getBlankScore,
+    IGame,
+    IGameDB,
+    IPlayerScore,
+    IThrowResult,
+    LocalStorageGameDB,
+} from "../../data/GamesDB";
 
 interface IProps {
-  gameId: number;
+    gameId: string;
 }
 
-type ISector = "Bull" | IDartValue;
-
-interface IPlayerScore {
-  points: number;
-  marks: {
-    [k in ISector]?: number;
-  };
-}
-
-interface IPlayer {
-  name: string;
-  score: IPlayerScore;
-}
-
-interface IThrowResult {
-  type: ISegmentType;
-  value: IDartValue | "Bull";
-}
-
-interface IState {
-  players: IPlayer[];
-  turnNumber: number;
-  turns: IThrowResult[][];
-}
-
-const getBlankScore = () => {
-  let score: IPlayerScore = {
-    points: 0,
-    marks: {}
-  };
-
-  return score;
-};
+interface IState {}
 
 export class CricketGamePlayPage extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
+    constructor(props: IProps) {
+        super(props);
 
-    this.state = {
-      players: [
-        {
-          name: "Indra",
-          score: getBlankScore()
-        },
-        {
-          name: "Jonathon",
-          score: getBlankScore()
-        }
-      ],
-      turnNumber: 0,
-      turns: [[]]
-    };
-  }
-
-  getDerivedProps() {
-    let playerIndex = this.state.turnNumber % this.state.players.length;
-
-    return {
-      round: Math.floor(
-        (this.state.turnNumber + 2) / this.state.players.length
-      ),
-      currentPlayer: this.state.players[playerIndex],
-      currentPlayerIndex: playerIndex
-    };
-  }
-
-  handleNextTurn() {
-    this.setState((curState) => {
-      return {
-        turns: [...curState.turns, []],
-        turnNumber: curState.turnNumber + 1
-      };
-    });
-  }
-
-  handleSegmentClick(curThrow: IThrowResult) {
-    if (curThrow.type === "Label") {
-      return; // do nothing
+        this.state = {};
     }
 
-    this.setState((curState) => {
-      let turns = [...curState.turns];
+    private getDB(): IGameDB {
+        return new LocalStorageGameDB();
+    }
 
-      let thisRound = turns[this.state.turnNumber] || [];
+    private getGame(): IGame {
+        let db = this.getDB();
 
-      if (thisRound.length === 3) {
-        thisRound[2] = curThrow;
-      } else {
-        thisRound.push(curThrow);
-      }
+        return db.getGames().filter((x) => x.id == this.props.gameId)[0];
+    }
 
-      turns[this.state.turnNumber] = thisRound;
+    getDerivedProps() {
+        let game = this.getGame();
+        let playerIndex = game.turnNumber % game.players.length;
 
-      return {
-        turns: turns
-      };
-    });
-  }
-
-  calculateScores(): IPlayerScore[] {
-    let playerScores: IPlayerScore[] = this.state.players.map(
-      (player, playerIndex) => {
         return {
-          points: 0,
-          marks: {}
+            round: Math.floor((game.turnNumber + 2) / game.players.length),
+            currentPlayer: game.players[playerIndex],
+            currentPlayerIndex: playerIndex,
         };
-      }
-    );
+    }
 
-    this.state.turns.forEach((turn, turnIndex) => {
-      let playerIndex = turnIndex % this.state.players.length;
-      let player = playerScores[playerIndex];
+    handleNextTurn() {
+        let game = this.getGame();
+        let newGame = {
+            ...game,
+            turns: [...game.turns, []],
+            turnNumber: game.turnNumber + 1,
+        };
 
-      for (var throwResult of turn) {
-        let count =
-          throwResult.type === "Double"
-            ? 2
-            : throwResult.type === "Treble"
-            ? 3
-            : 1;
+        this.getDB().saveGame(newGame);
+        this.forceUpdate();
+    }
 
-        let resultForValue = player.marks[throwResult.value] || 0;
-        Array(count)
-          .fill("")
-          .forEach(() => {
-            if (resultForValue === 3) {
-              player.points = player.points + parseInt(throwResult.value);
-            } else {
-              resultForValue = resultForValue + 1;
+    handleSegmentClick(curThrow: IThrowResult) {
+        if (curThrow.type === "Label") {
+            return; // do nothing
+        }
+
+        let game = this.getGame();
+
+        let turns = [...game.turns];
+
+        let thisRound = turns[game.turnNumber] || [];
+
+        if (thisRound.length === 3) {
+            thisRound[2] = curThrow;
+        } else {
+            thisRound.push(curThrow);
+        }
+
+        turns[game.turnNumber] = thisRound;
+
+        game.turns = turns;
+
+        this.getDB().saveGame(game);
+
+        this.forceUpdate();
+    }
+
+    calculateScores(): IPlayerScore[] {
+        let game = this.getGame();
+        let playerScores: IPlayerScore[] = game.players.map(
+            (player, playerIndex) => {
+                return {
+                    points: 0,
+                    marks: {},
+                };
             }
-          });
+        );
 
-        player.marks[throwResult.value] = resultForValue;
-      }
-    });
+        game.turns.forEach((turn, turnIndex) => {
+            let playerIndex = turnIndex % game.players.length;
+            let player = playerScores[playerIndex];
 
-    return playerScores;
-  }
+            for (var throwResult of turn) {
+                let count =
+                    throwResult.type === "Double"
+                        ? 2
+                        : throwResult.type === "Treble"
+                        ? 3
+                        : 1;
 
-  render() {
-    let { props } = this;
-    let derivedState = this.getDerivedProps();
+                let resultForValue = player.marks[throwResult.value] || 0;
+                Array(count)
+                    .fill("")
+                    .forEach(() => {
+                        if (resultForValue === 3) {
+                            player.points =
+                                player.points + parseInt(throwResult.value);
+                        } else {
+                            resultForValue = resultForValue + 1;
+                        }
+                    });
 
-    let currentTurn = this.state.turns[this.state.turnNumber];
+                player.marks[throwResult.value] = resultForValue;
+            }
+        });
 
-    let currentScores = this.calculateScores();
+        return playerScores;
+    }
 
-    return (
-      <div>
-        <h2>
-          <Link to="/">&lt; Home</Link> Game {props.gameId}
-        </h2>
+    render() {
+        let { props } = this;
+        let game = this.getGame();
+        let derivedState = this.getDerivedProps();
 
-        <h3>Current Round: {derivedState.round}</h3>
-        <h3>Current Player: {derivedState.currentPlayer.name}</h3>
+        let currentTurn = game.turns[game.turnNumber];
 
-        <div style={{ display: "flex" }}>
-          <div>
-            <Dartboard
-              height="400"
-              width="400"
-              onSegmentClick={this.handleSegmentClick.bind(this)}
-            />
-            <button
-              onClick={() => {
-                this.setState((curState) => {
-                  let turns = [...curState.turns];
+        let currentScores = this.calculateScores();
 
-                  turns[this.state.turnNumber] = turns[
-                    this.state.turnNumber
-                  ].slice(0, -1);
+        return (
+            <div>
+                <h2>
+                    <Link to="/">&lt; Home</Link> Game {props.gameId}
+                </h2>
 
-                  return {
-                    turns: turns
-                  };
-                });
-              }}
-            >
-              Unthrow
-            </button>
-            <button onClick={this.handleNextTurn.bind(this)}>Next Round</button>
-          </div>
-          <div>
-            <div style={{ display: "flex" }}>
-              {currentTurn.map((round, i) => {
-                return (
-                  <div style={{ outline: "blue", margin: 10 }}>
-                    <h4>{i + 1}</h4>
-                    <h5>
-                      {round.value} / {round.type}
-                    </h5>
-                  </div>
-                );
-              })}
+                <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <Card>
+                        <Card.Title>{derivedState.round}</Card.Title>
+                        <Card.Subtitle>Current Round</Card.Subtitle>
+                    </Card>
+
+                    <div className="card mb-3" style={{ maxWidth: 540 }}>
+                        <div className="row no-gutters">
+                            <div className="col-md-4">
+                                <img
+                                    src={
+                                        derivedState.currentPlayer.playerInfo
+                                            .profilePic
+                                    }
+                                    className="card-img"
+                                    alt="..."
+                                />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        Currently Throwing
+                                    </h5>
+                                    <p className="card-text">
+                                        {
+                                            derivedState.currentPlayer
+                                                .playerInfo.name
+                                        }
+                                    </p>
+                                    <p className="card-text">
+                                        <small className="text-muted">
+                                            Last updated 3 mins ago
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div style={{ display: "flex" }}>
+                    <Dartboard
+                        height="550"
+                        width="550"
+                        onSegmentClick={this.handleSegmentClick.bind(this)}
+                    />
+
+                    <div>
+                        <div style={{ display: "flex" }}>
+                            {[0, 1, 2].map((roundIndex, i) => {
+                                let round = currentTurn[roundIndex];
+                                return (
+                                    <div
+                                        style={{
+                                            outline: "blue",
+                                            margin: 10,
+                                            width: 135,
+                                            height: 70,
+                                        }}
+                                    >
+                                        <h4>{i + 1}</h4>
+                                        {round && (
+                                            <h5>
+                                                {round.value} / {round.type}
+                                            </h5>
+                                        )}
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+                        <div style={{ display: "flex" }}>
+                            <Button
+                                onClick={() => {
+                                    let game = this.getGame();
+
+                                    let turns = [...game.turns];
+
+                                    game.turns = turns.slice(0, -1);
+                                    game.turnNumber = game.turnNumber - 1;
+
+                                    this.getDB().saveGame(game);
+                                    this.forceUpdate();
+                                }}
+                            >
+                                Go To Previous Turn
+                            </Button>
+                            <Button
+                                onClick={() => {
+                                    let game = this.getGame();
+
+                                    let turns = [...game.turns];
+
+                                    turns[game.turnNumber] = turns[
+                                        game.turnNumber
+                                    ].slice(0, -1);
+
+                                    game.turns = turns;
+
+                                    this.getDB().saveGame(game);
+                                    this.forceUpdate();
+                                }}
+                            >
+                                Unthrow
+                            </Button>
+                            <Button onClick={this.handleNextTurn.bind(this)}>
+                                Next Round
+                            </Button>
+                        </div>
+
+                        <hr />
+
+                        <div style={{ display: "flex" }}>
+                            {game.players.map((player, playerIndex) => {
+                                let score = currentScores[playerIndex];
+
+                                let isCurrentPlayer =
+                                    this.getDerivedProps().currentPlayer
+                                        .playerInfo.id === player.playerInfo.id;
+
+                                return (
+                                    <div
+                                        style={{
+                                            outline: isCurrentPlayer
+                                                ? "1px solid blue"
+                                                : undefined,
+                                            borderRadius: 10,
+                                            margin: 10,
+                                            padding: 10,
+                                        }}
+                                    >
+                                        <h4>{player.playerInfo.name}</h4>
+                                        <h5>Score: {score.points}</h5>
+
+                                        <ol>
+                                            {[
+                                                "15",
+                                                "16",
+                                                "17",
+                                                "18",
+                                                "19",
+                                                "20",
+                                            ].map((segmentId) => {
+                                                let count =
+                                                    (score.marks as any)[
+                                                        segmentId
+                                                    ] || 0;
+
+                                                let display =
+                                                    count === 1
+                                                        ? "∕"
+                                                        : count === 2
+                                                        ? "✕"
+                                                        : count === 0
+                                                        ? " "
+                                                        : count === 3
+                                                        ? "⦻"
+                                                        : "?";
+
+                                                return (
+                                                    <li
+                                                        style={{ fontSize: 28 }}
+                                                    >
+                                                        {segmentId}: {display}
+                                                    </li>
+                                                );
+                                            })}
+                                        </ol>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div style={{ display: "flex" }}>
-              {this.state.players.map((player, playerIndex) => {
-                let score = currentScores[playerIndex];
-
-                return (
-                  <div style={{ outline: "blue", margin: 10 }}>
-                    <h4>{player.name}</h4>
-                    <h5>Score: {score.points}</h5>
-
-                    <ol>
-                      {["15", "16", "17", "18", "19", "20"].map((segmentId) => {
-                        let count = (score.marks as any)[segmentId] || 0;
-
-                        let display =
-                          count === 1
-                            ? "∕"
-                            : count === 2
-                            ? "✕"
-                            : count === 0
-                            ? " "
-                            : count === 3
-                            ? "⦻"
-                            : "?";
-
-                        return (
-                          <li>
-                            {segmentId}: {display}
-                          </li>
-                        );
-                      })}
-                    </ol>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
